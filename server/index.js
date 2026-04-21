4c0934911e79a9f3392a1426392ef16973586c60
KalpanaPatidar <163427005+KalpanaPatidar@users.noreply.github.com>
2026-04-21 00:20:43 -0700

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import isAuth from "./middlewares/isAuth.js";
 dotenv.config()
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interviewai-project-client-1h36.onrender.com",
     credentials: true
 }))
 app.use(express.json())
